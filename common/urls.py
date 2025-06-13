5e5506aca07997387d7c1beb66fdf5df3d123996
rahmatkulov <rahmatkulovmuhammadjon18@gmail.com>
2025-06-13 12:11:04 +0500

    Views, urls, list templates

--- common/urls.py
+++ common/urls.py
@@ -4,4 +4,10 @@ app_name = "common"
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+
+    path('group-list', views.GroupListView.as_view(), name='group-list'),
+    path('group-create', views.GroupCreateView.as_view(), name='group-create'),
+    path('group/<int:pk>/create', views.GroupUpdateView.as_view(), name='group-update'),
+    path('group/<int:pk>/update', views.GroupDeleteView.as_view(), name='group-delete'),
+
 ]
--- common/views.py
+++ common/views.py
@@ -1,5 +1,42 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import ListView
+from . import models
+
+
+from helpers.views import CreateView, UpdateView, DeleteView 
+
+
 class HomeView(View):
     def get(self, request):
         return render(request, 'index.html')
+
+class GroupListView(ListView):
+    model = models.Group
+    template_name = "groups/list.html"
+
+    context_object_name = "objects"
+    queryset = model.objects.all().order_by("-id")
+    paginate_by = 10
+
+class GroupCreateView(CreateView):
+    model = models.Group
+    template_name = "groups/create.html"
+    context_object_name = "objects"
+    success_url = "common:group-list"
+    success_create_url = "common:group-create"
+
+
+class GroupUpdateView(UpdateView):
+    model = models.Group
+    template_name = "groups/update.html"
+    context_object_name = "objects"
+    success_url = "common:group-list"
+    success_update_url = "common:group-update"
+
+
+class GroupDeleteView(DeleteView):
+    model = models.Group
+    success_url = "common:group-list"
+
+
